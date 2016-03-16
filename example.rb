49469072d4534bb067c8ab22195400905e86dcf1
Robert Ignasiak <tori@robert-i.com>
2016-03-16 15:14:06 +0100

    Example database with types

--- example.rb
+++ example.rb
@@ -1,26 +1,68 @@
 require "rubygems"
 require "sequel"
+require "faker"
 
-# connect to an in-memory database
-DB = Sequel.sqlite
+DB = Sequel.connect("postgres://localhost/budget")
 
-# create an items table
-DB.create_table :items do
+DB.drop_table(:users, :accounts, :reminders, :currencies, :prices, :recurring_infos, :transactions, :cascade=>true)
+
+DB.create_table :users do
   primary_key :id
   String :name
-  Float :price
+  Integer :account_number
+  Integer :phone
+  String :note
 end
 
-# create a dataset from the items table
-items = DB[:items]
+DB.create_table :accounts do
+	primary_key :id
+	String :name
+	Integer :balance
+end
 
-# populate the table
-items.insert(:name => 'abc', :price => rand * 100)
-items.insert(:name => 'def', :price => rand * 100)
-items.insert(:name => 'ghi', :price => rand * 100)
+DB.create_table :reminders do 
+	primary_key :id
+	String :name
+	String :description
+end
 
-# print out the number of records
-puts "Item count: #{items.count}"
+DB.create_table :currencies do
+	primary_key :id
+	String :name
+	String :short_name
+	DateTime :date_time
+	Integer :current_value_in_usd
+end
 
-# print out the average price
-puts "The average price is: #{items.avg(:price)}"
+DB.create_table :prices do
+	primary_key :id
+	Integer :value
+end
+
+DB.create_table :recurring_infos do
+	primary_key :id
+	DateTime :starting_date
+	DateTime :end_date
+	String :period
+end
+
+DB.create_table :transactions do
+	primary_key :id
+	String :description
+	String :place
+	String :transaction_type
+	TrueClass :is_archive
+end
+
+DB.create_table :payees do
+	primary_key :id
+	String :name
+	Integer :account_number
+	Integer :phone
+	String :note
+end
+
+DB.create_table :limits do
+	primary_key :id
+	Integer :amount
+end
