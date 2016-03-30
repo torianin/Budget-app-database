e814841659ba96534a0b5b4291d3433632e95048
Szymon Koper <sz.koper@gmail.com>
2016-03-30 03:59:30 +0200

    Updated clear.rb

--- clear.rb
+++ clear.rb
@@ -1,5 +1,11 @@
 require "sequel"
 
-DB = Sequel.connect("postgres://localhost/budget")
+DB = Sequel.connect("postgres://postgres:123@localhost/budget")
 
-DB.drop_table(:tags, :categories, :users, :accounts, :reminders, :currencies, :prices, :recurring_infos, :transactions, :payees, :limits, :cascade=>true)
+tables = [:categories_tags, :tags, :categories, :users, :accounts, :reminders, :currencies, :prices, :recurring_infos, :transactions, :payees, :limits]
+
+tables.each do |table|
+	if DB.table_exists?(table)
+		DB.drop_table(table, :cascade => true)
+	end
+end
